863c024465d2e0a83a7ea6bbb830523a9bd4122a
Varuub <varunub1505@gmail.com>
2023-09-07 01:37:39 +0530

    Initialized weather forecast API

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express=require('express');
+require("dotenv").config()
 const app = express();
+const https=require('https');
+const request = require('request');
 
+
+const API_KEY=process.env.API_KEY;
+const city="Dandeli"
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
 
 
 app.get('/',(req,res)=>{
-    res.send("<h1>Hiii</h1>")
+  // var jsonfile = getWeather("Belgaum")
+  // console.log(weatherdata);
+  https.get(url,response=>{
+    response.on("data",(data)=>{
+      const weatherdata=JSON.parse(data)
+      // console.log(weatherdata.weather)
+      res.send(weatherdata);
+    })
+    
+    // console.log(weatherdata.weather)
+    // res.send(weatherdata.weather[0].description);
+  })
+  
 })
 app.listen(3000,()=>{
     console.log("Server Started at port 3000");
